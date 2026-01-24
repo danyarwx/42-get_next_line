ded93bd9626ec07a7297350abff8e86843bd5b62
Danila Zhukov <danila@Darkbook.local>
2026-01-24 11:43:36 +0100

    Implement buffer copying

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dzhukov <dzhukov@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 15:11:02 by dzhukov           #+#    #+#             */
-/*   Updated: 2026/01/22 17:49:42 by dzhukov          ###   ########.fr       */
+/*   Updated: 2026/01/24 11:28:48 by dzhukov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,87 @@
 #include <stdio.h>
 #include <fcntl.h>
 
+#include "get_next_line.h"
 # define BUFFER_SIZE 9
 
 
-#include "get_next_line.h"
+char	*ft_strchr(const char *s, int c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char)c)
+			return ((char *)(s + i));
+		i++;
+	}
+	if (s[i] == (char)c)
+		return ((char *)(s + i));
+	return (NULL);
+}
+
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len1;
+	size_t	i;
+	char	*ptr;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len1 = ft_strlen(s1);
+	ptr = (char *)malloc((len1 + ft_strlen(s2) + 1) * sizeof(char));
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	while (i < len1)
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (i < ft_strlen(s2))
+	{
+		ptr[i + len1] = s2[i];
+		i++;
+	}
+	ptr[i + len1] = '\0';
+	return (ptr); // Returns allocated string
+}
+
+
+
+
+
+
 char *get_next_line(int fd)
 {
+	static char *stash;
+	char *line;
+	int bytes_read;
 	char *buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buffer)
 		return (NULL);
 
-	int bytes_read = read(fd, buffer, BUFFER_SIZE);
 
-	if(bytes_read <= 0)
+	bytes_read = 1;
+	while (ft_strchr(stash, '\n') != NULL && bytes_read != 0)
 	{
-		printf("Read function failed.\n");
-		free(buffer);
-		return (NULL);
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if(bytes_read <= 0)
+		{
+			printf("Read function failed.\n");
+			free(buffer);
+			return (NULL);
+		}
+		buffer[bytes_read] = '\0';
+		stash = ft_strjoin(stash, buffer);
 	}
 
-	buffer[bytes_read] = '\0';
-	return(buffer);
+
+	free(buffer);
+	return(line);
 }
 
 int main(void)
@@ -51,7 +111,7 @@ int main(void)
 		return 1;
 	}
 
-	printf("File 'newfile.txt' created successfully \nFile descriptor: %d\n", fd);
+	printf("File created successfully \nFile descriptor: %d\n", fd);
 
 	char *str;
 	str = get_next_line(fd);
