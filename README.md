3434d200bedcc4f41e86e66797ce906bce0bf09d
Danila Zhukov <danila@Darkbook.local>
2026-01-29 18:47:38 +0100

    Move code explanation from gnl to README

--- README.md
+++ README.md
@@ -48,3 +48,62 @@ The selected algorithm utilizes a static character pointer to maintain a *"stash
 
 3. **Efficiency with Buffer Sizes:** While linked lists can be efficient for very large buffer sizes to avoid massive realloc operations, a string-based approach is more straightforward for the typical `BUFFER_SIZE` ranges.
 
+### Detailed Explanation
+
+We have a file with multiple lines:
+
+```bash
+Line One\n
+Line Two\n
+Line Three\n
+```
+
+And the BUFFER_SIZE set to 5 initially
+
+How get_next_line works:
+
+The main function reads the file with a given buffer and puts it
+into a string buffer.
+Buffer gets overwritten with each call.
+Now we uppend each of the read outputs into another string called stash,
+using ft_strjoin and freeing the previous stash all the time.
+And with each iteration we check whether or not this stash contains '\n'
+
+As soon as newline is inside the string, we stop the loop,
+we free the buffer, and we return the stash containing either just our first string or a little more.
+
+Next step is to extract the exact line we need.
+With the extract_line function we allocate memory and make a copy of the string just till the newline symbol or till end of string is reached, this is stored in "line".
+
+Next step is to update stash in case we have something left.
+Here we basically make another stash and copy there from the newline
+
+```c
+if (stash[i] == '\0')
+{
+    free(stash);
+    return (NULL);
+}
+```
+
+What it does:
+- Check if there's any content after the `\n`
+- If the character right after `\n` is `\0`, we've extracted everything
+
+Example 1**: `stash = "OneLine\n"`
+```bash
+Index: 0 1 2 3 4 5 6 7 8
+Char:  O n e L i n e \n \0
+
+After i++: i = 8
+Check: stash[8] == '\0'? → YES
+
+Action:
+  1. free(stash) → release memory
+  2. return NULL → nothing left
+```
+
+We either update stash by copying everything after the newline, or we check and if there is not anything after the newline or EOF is reached, we free stash and return NULL;
+
+Finally we return the line that was read.
+If we run get next line again, the leftovers are stored in the static variable stash and we are going to print them out next time.
--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dzhukov <dzhukov@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 15:11:02 by dzhukov           #+#    #+#             */
-/*   Updated: 2026/01/29 18:10:59 by dzhukov          ###   ########.fr       */
+/*   Updated: 2026/01/29 18:44:35 by dzhukov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -152,68 +152,3 @@ char	*get_next_line(int fd)
 // 	close(fd);
 // 	return (0);
 // }
-//
-// We have a file with multiple lines:
-
-// Line One\n
-// Line Two\n
-// Line Three\n
-
-// And the buffer with size 5
-
-// How get next line works:
-
-// The main function reads the file with a given buffer and puts it
-// into a string buffer
-// buffer gets overwritten with each call.
-// Now we uppend each of the read outputs into another string called stash,
-// using ft_strjoin and freeing the previous stash all the time.
-// And with each iteration we check whether or not this stash contains '\n'
-
-// As soon as newline is inside the string, we stop the loop,
-// we free the buffer,
-// and we return the stash containing either just our first string
-// or a little more.
-
-// Next step is to extract the exact line we need.
-// with the extract_line function
-// we allocate memory and make a copy of the string just till
-// the newline symbol
-// or till end of string is reached, this is stored in "line".
-
-// Next step is to update stash in case we have something left.
-// Here we basically
-// make another stash and copy there from the newline
-
-// if (stash[i] == '\0')
-// {
-//     free(stash);
-//     return (NULL);
-// }
-// ```
-
-// What it does:
-// - Check if there's any content after the `\n`
-// - If the character right after `\n` is `\0`, we've extracted everything
-
-// Example 1**: `stash = "OneLine\n"`
-// ```
-// Index: 0 1 2 3 4 5 6 7 8
-// Char:  O n e L i n e \n \0
-
-// After i++: i = 8
-// Check: stash[8] == '\0'? → YES
-
-// Action:
-//   1. free(stash) → release memory
-//   2. return NULL → nothing left
-// ```
-
-// We either update stash by copying everything after the newline, or we check
-// and if there is not anything after the newline or EOF is reached,
-// we free stash and return NULL;
-
-// Finally we return the line that was read.
-// If we run get next line again, the leftovers are stored
-// in the static variable
-// stash and we are going to print them out next time.
